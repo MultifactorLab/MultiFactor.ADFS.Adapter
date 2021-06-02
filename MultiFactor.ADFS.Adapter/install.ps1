7be49f588e48a72d871366d6e73eb56f919c5a49
Константин Ян <konstantin.yan@gmail.com>
2021-06-02 11:12:06 +0300

    Install & uninstall scripts improvements

--- MultiFactor.ADFS.Adapter/install.ps1
+++ MultiFactor.ADFS.Adapter/install.ps1
@@ -1,18 +1,35 @@
+# MultiFactor ADFS Adapter install script
+# Run as Administrator on every ADFS server
+
 $path = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 Set-Location $path
+
+# 1. Add adapter assembly to global cache
 
 [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
 $publish = New-Object System.EnterpriseServices.Internal.Publish
 
 $publish.GacInstall($path + '\MultiFactor.ADFS.Adapter.dll')
 
+# 2. Register ADFS authentication provider on master
+
 $fn = ([System.Reflection.Assembly]::LoadFile($path + '\MultiFactor.ADFS.Adapter.dll')).FullName
 
 $typeName = "MultiFactor.ADFS.Adapter.AuthenticationAdapter, " + $fn.ToString() + ", processorArchitecture=MSIL"
-Register-AdfsAuthenticationProvider -TypeName $typeName -Name "MultiFactor" -ConfigurationFilePath ($path + '\MultiFactor.ADFS.Adapter.dll.config')
+
+$stsrole = Get-ADFSSyncProperties | Select-Object -ExpandProperty Role
+if ($stsrole -eq "PrimaryComputer") {
+	Register-AdfsAuthenticationProvider -TypeName $typeName -Name "MultiFactor" -ConfigurationFilePath ($path + '\MultiFactor.ADFS.Adapter.dll.config')
+}
+
+# 3. Restart ADFS service
 
 net stop adfssrv
 net start adfssrv
 
-New-EventLog -LogName Application -Source MultiFactor
+# 4. Create Application EventLog source MultiFactor if not exists
+
+if (!([System.Diagnostics.EventLog]::SourceExists("MultiFactor"))){
+	New-Eventlog -LogName "Application" -Source "MultiFactor"
+}
--- MultiFactor.ADFS.Adapter/uninstall.ps1
+++ MultiFactor.ADFS.Adapter/uninstall.ps1
@@ -1,7 +1,17 @@
+# MultiFactor ADFS Adapter uninstall script
+# Run as Administrator on every ADFS server
+
 $path = Split-Path $MyInvocation.MyCommand.Path -Parent
 Set-Location $path
 
-Unregister-AdfsAuthenticationProvider -Name "MultiFactor"
+# 1. Unregister ADFS authentication provider on master
+
+$stsrole = Get-ADFSSyncProperties | Select-Object -ExpandProperty Role
+if ($stsrole -eq "PrimaryComputer") {
+	Unregister-AdfsAuthenticationProvider -Name "MultiFactor"
+}
+
+# 2. Remove adapter assembly from global cache
 
 [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
 $publish = New-Object System.EnterpriseServices.Internal.Publish
